a078b941155e3732c6e375f16fbf1300f2b8cb60
Vinicius Augusto <viniciguto1@gmail.com>
2022-11-26 11:50:33 -0300

    Service

--- src/app/service/produtoService.ts
+++ src/app/service/produtoService.ts
@@ -12,15 +12,15 @@ export class ProdutoService{
 constructor(private http: HttpClient){}
 
     save(produto: Produto): Observable<any> {
-        return this.http.post<Produto>(this.url = 'produto', produto)
+        return this.http.post<Produto>(this.url + 'produto', produto)
     }
 
     getProduto(): Observable<Produto[]>{
-        return this.http.get<Produto[]>(this.url = 'produto');
+        return this.http.get<Produto[]>(this.url + 'produto');
     }
 
     atualizar(produto: Produto): Observable<any>{
-        return this.http.put<Produto>(this.url = '/${produto.id}', produto);
+        return this.http.put<Produto>(this.url + '/${produto.id}', produto);
       }
 
 }
